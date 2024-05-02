e4bcc212c68fdde397e9cc1c22ea722a3b2828ec
Krishna Chaitanya <chaitu@Kaysee-MacBookPro.local>
2024-05-03 00:38:02 +0530

    Initial setup with simple controller

---
+++ src/main/java/com/kaysee/pilot/controller/PilotController.java
@@ -0,0 +1,19 @@
+package com.kaysee.pilot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PilotController {
+
+    @GetMapping("/health-check")
+    public Boolean getHealthCheck() {
+        return true;
+    }
+
+    @GetMapping("/")
+    public String getMessage() {
+        return "Welcome!!";
+    }
+
+}
